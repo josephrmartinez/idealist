e734a6c6a24ce41df0f3fc4d2f66a1c623477863
Joseph Martinez <joseph.r.martinez@gmail.com>
2022-11-22 18:12:39 -0700

    functional programming demo

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("const divvy = document.getElementsByTagName('div')\n\n//# sourceURL=webpack://idealist/./src/index.js?");
+eval("// import createTopic and addIdea from functions/logic\n\nlet topics = [\"summer camps\", \"weekend coding projects\"]\n\n// on load: renderTopics(), renderBlankIdeasPanel()\n\n\n\nconst newTopicBtn = document.getElementById('newTopicBtn')\nconst topicList = document.getElementById('topicList')\n\nconst topic = document.createElement('div')\ntopic.classList.add('topic')\n\nnewTopicBtn.addEventListener('click', createTopicInput)\n\n\n\nfunction renderTopics(){\n    topics.forEach(item => {\n        topic.innerText = item; \n        topicList.appendChild(topic);\n    })\n}\n\nfunction createTopicInput(){\n    let newTopicInput = document.createElement('input')\n    newTopicInput.type.add('text');\n    newTopicInput.classList.add('topicInput');\n    newTopicInput.id = 'topicInput'\n    newTopicInput.addEventListener(\"keyup\", addNewTopic)\n    topicList.appendChild(newTopicInput)\n}\n\n\nrenderTopics()\n\n\nfunction addNewTopic() {\n    let newTopicInputText = document.getElementById('topicInput').value;\n    topics.push(newTopicInputText)\n    renderTopics()\n}\n\n\n\n\n// const addNewTopic = (inputText) => {\n//     topics.push({\n//         topic,\n//         ideaList: [],\n//     })\n// }\n\n    // createTopicInput();\n\n    // add new div in the form of an input field, add enter functionality to this field\n    // upon enter, add input to topics list\n    // then call render function to turn input field into div\n\n\n\n\n\n\n\n//# sourceURL=webpack://idealist/./src/index.js?");
 
 /***/ })
 
--- src/index.js
+++ src/index.js
@@ -1,1 +1,64 @@
-const divvy = document.getElementsByTagName('div')
+// import createTopic and addIdea from functions/logic
+
+let topics = ["summer camps", "weekend coding projects"]
+
+// on load: renderTopics(), renderBlankIdeasPanel()
+
+
+
+const newTopicBtn = document.getElementById('newTopicBtn')
+const topicList = document.getElementById('topicList')
+
+const topic = document.createElement('div')
+topic.classList.add('topic')
+
+newTopicBtn.addEventListener('click', createTopicInput)
+
+
+
+function renderTopics(){
+    topics.forEach(item => {
+        topic.innerText = item; 
+        topicList.appendChild(topic);
+    })
+}
+
+function createTopicInput(){
+    let newTopicInput = document.createElement('input')
+    newTopicInput.type.add('text');
+    newTopicInput.classList.add('topicInput');
+    newTopicInput.id = 'topicInput'
+    newTopicInput.addEventListener("keyup", addNewTopic)
+    topicList.appendChild(newTopicInput)
+}
+
+
+renderTopics()
+
+
+function addNewTopic() {
+    let newTopicInputText = document.getElementById('topicInput').value;
+    topics.push(newTopicInputText)
+    renderTopics()
+}
+
+
+
+
+// const addNewTopic = (inputText) => {
+//     topics.push({
+//         topic,
+//         ideaList: [],
+//     })
+// }
+
+    // createTopicInput();
+
+    // add new div in the form of an input field, add enter functionality to this field
+    // upon enter, add input to topics list
+    // then call render function to turn input field into div
+
+
+
+
+
