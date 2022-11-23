a0bf5ef979b5ba725f0a58bfb359f5c6fa2519c1
Joseph Martinez <joseph.r.martinez@gmail.com>
2022-11-22 19:02:41 -0700

    functional approach working, ready for refactor

--- src/index.js
+++ src/index.js
@@ -2,45 +2,53 @@
 
 let topics = ["summer camps", "weekend coding projects"]
 
-// on load: renderTopics(), renderBlankIdeasPanel()
-
-
 
 const newTopicBtn = document.getElementById('newTopicBtn')
 const topicList = document.getElementById('topicList')
 
-const topic = document.createElement('div')
-topic.classList.add('topic')
-
 newTopicBtn.addEventListener('click', createTopicInput)
 
 
-
 function renderTopics(){
+    topicList.innerHTML = '';
     topics.forEach(item => {
+        const topic = document.createElement('div')
+        topic.classList.add('topic')
         topic.innerText = item; 
         topicList.appendChild(topic);
     })
 }
 
 function createTopicInput(){
+    console.log('function called')
     let newTopicInput = document.createElement('input')
-    newTopicInput.type.add('text');
+    newTopicInput.type = 'text';
     newTopicInput.classList.add('topicInput');
     newTopicInput.id = 'topicInput'
-    newTopicInput.addEventListener("keyup", addNewTopic)
+    newTopicInput.addEventListener("keydown", addNewTopic)
     topicList.appendChild(newTopicInput)
+    newTopicInput.focus()
+}
+
+
+function addNewTopic(eventObj) {
+    if (eventObj.keyCode===13) {
+        let newTopicInputText = document.getElementById('topicInput').value;
+        topics.push(newTopicInputText)
+        renderTopics()
+    }
+    
 }
 
 
 renderTopics()
 
 
-function addNewTopic() {
-    let newTopicInputText = document.getElementById('topicInput').value;
-    topics.push(newTopicInputText)
-    renderTopics()
-}
+
+
+
+
+
 
 
 
